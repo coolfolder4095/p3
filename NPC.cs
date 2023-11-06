67b6930478f3f6340175eb993137ca6cb3bab94f
coolfolder4095 <148997045+coolfolder4095@users.noreply.github.com>
2023-11-06 18:59:06 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+   public int health = 5;
+    public int level = 1;
+    public float speed = 1.2f;
+    void Start()
+    {
+        health = health + level;
+        print("Жизней: " + health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Создаём переменную вида Vector3 и сохраняем в неё позицию NPC
+        Vector3 newPosition = transform.position;
+
+        //Меняем позицию NPC по оси z согласно скорости NPC и времени между кадрами
+        newPosition.z += speed * Time.deltaTime;
+
+        //Меняем позицию NPC на новое значение, рассчитанное выше
+        transform.position = newPosition;
+    }
+}
